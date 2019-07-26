a3f20a5af867a79f28bddf71c20b3696106e7cbb
GetALife <1340482017@qq.com>
2019-07-27 01:28:33 +0800

    :beers: update huaweioj

--- huawei/RepresentNum.java
+++ huawei/RepresentNum.java
@@ -3,6 +3,29 @@ package tk.solidays.algorithm.huawei;
 import java.io.BufferedInputStream;
 import java.util.Scanner;
 
+/**
+ * 题目描述
+ * 将一个字符中所有出现的数字前后加上符号“*”，其他字符保持不变
+ * public static String MarkNum(String pInStr)
+ * {
+ *
+ * return null;
+ * }
+ * 注意：输入数据可能有多行
+ * 输入描述:
+ * 输入一个字符串
+ *
+ * 输出描述:
+ * 字符中所有出现的数字前后加上符号“*”，其他字符保持不变
+ *
+ * 示例1
+ * 输入
+ * 复制
+ * Jkdi234klowe90a3
+ * 输出
+ * 复制
+ * Jkdi*234*klowe*90*a*3*
+ */
 public class RepresentNum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(new BufferedInputStream(System.in));
@@ -14,9 +37,13 @@ public class RepresentNum {
                     sb.insert(i,'*');
                     flag=false;
                 }
-                if(!flag&&(sb.charAt(i)<'0'||sb.charAt(i)>'9')){
+                else if(!flag&&(sb.charAt(i)<'0'||sb.charAt(i)>'9')){
                     sb.insert(i,'*');
                     flag=true;
+                }
+                else if(i==sb.length()-1&&sb.charAt(i)>='0'&&sb.charAt(i)<='9'){
+                    sb.append('*');
+                    break;
                 }
             }
             System.out.println(sb);

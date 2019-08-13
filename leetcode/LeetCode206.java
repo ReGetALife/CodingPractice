75a10eb3b6ce0a251732c4fb7f5cbb2f00987899
GetALife <1340482017@qq.com>
2019-08-13 14:32:20 +0800

    :beers: update leetcode

--- leetcode/LeetCode206.java
+++ leetcode/LeetCode206.java
@@ -1,5 +1,19 @@
 package tk.solidays.algorithm.leetcode;
 
+/**
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LeetCode206 {
     //迭代法，从头开始拆节点，让下一个节点反过来指向它
     //递归法，当前链表的反转链表等于头节点下一个节点的反转链表加上头节点

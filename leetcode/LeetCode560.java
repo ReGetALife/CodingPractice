f5e87e6f9125153cff650dd27ef621b783df7006
GetALife <1340482017@qq.com>
2019-10-07 01:26:21 +0800

    :beers: update leecode

---
+++ leetcode/LeetCode560.java
@@ -0,0 +1,35 @@
+package tk.solidays.algorithm.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ * <p>
+ * 示例 1 :
+ * <p>
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * 说明 :
+ * <p>
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeetCode560 {
+    //借鉴leetcode437中的法三，采用map计数
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int ans = 0, sum = 0;
+        for (int num : nums) {
+            sum += num;
+            ans += map.getOrDefault(sum - k, 0);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return ans;
+    }
+}

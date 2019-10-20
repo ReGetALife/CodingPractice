23e301a45f82fb822850c866251d8fbc16086580
GetALife <1340482017@qq.com>
2019-10-20 23:44:25 +0800

    :beers: update leetcode

--- leetcode/LeetCode324.java
+++ leetcode/LeetCode324.java
@@ -1,6 +1,10 @@
 package tk.solidays.algorithm.leetcode;
 
 public class LeetCode324 {
+    //找到中位数后进行一次划分。
+    //中位数可以通过排序找，也可通过快速选择找，最好是用快速选择的改进版bfprt算法（中位数的中位数算法，解决top k问题）
+    //bfprt改进了基准了选择，采用中位数作为基准
+    //下面采用快速选择方法，耗时
     public void wiggleSort(int[] nums) {
 
     }
